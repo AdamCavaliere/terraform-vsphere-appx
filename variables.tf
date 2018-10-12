6f68572f9c1c6e1d3a08c21c38ff7d779bc993b3
Adam Cavaliere <adam@mbp.local>
2018-10-12 14:26:02 -0500

    First pass

--- variables.tf
+++ variables.tf
@@ -1,7 +1,22 @@
-variable "disk_size" {}
-
-variable "vm_name" {}
-variable "cpu_count" {}
-variable "memory" {}
-variable "vm_count" {}
-variable "tag_name" {}
+variable "disk_size" {
+	description = "Size in GB of the Root Disk"
+	default = "35"
+}
+variable "vm_name" {
+	description = "Application Name"
+}
+variable "cpu_count" {
+	description = "Number of CPUs"
+	default = "2"
+}
+variable "memory" {
+	description = "Memory size in MB"
+	default = "1024"
+}
+variable "vm_count" {
+	description = "Number of VMs to create for this application"
+}
+variable "tag_name" {
+	description = "Tag to include, values can be Production or Staging"
+	default = "Staging"
+}
